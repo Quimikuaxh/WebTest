5dfb1e1a84bc5c7df19cb40a4433046954e8058d
adelavina <adriandlv53@gmail.com>
2022-08-30 13:18:00 +0200

    structured pages

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const app = express()
 const path = require('path');
 const port = process.env.PORT || 3000;
 
+app.use("/", express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'/index.html'));
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname,'/index.html'));
+// })
 
-app.get('/iframe.html', (req, res) => {
-    res.sendFile(path.join(__dirname,'/iframe.html'));
-})
+// app.get('/iframe.html', (req, res) => {
+//     res.sendFile(path.join(__dirname,'/iframe.html'));
+// })
 
 app.listen(port, () => {
     console.log(`Cypress app listening on port ${port}`)
